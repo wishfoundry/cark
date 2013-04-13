c65fa13754a126f7e4762b51646caacef5cec16f
wishoundry <bngreer@gmail.com>
2013-04-13 15:45:19 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cark::Application.config.secret_token = 'a4f017f67be455cfa92cdb0c5bd60a4ed98bad94ca8d1ea75b76e07ccc2d982270cfb381e7917fe7f292d2af1afe3ec994b0ad6b5a9f6784d3e732797930595b'
